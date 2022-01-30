df29b7cf19de38fe113301b1e3c824e8bff0b867
nana0519 <95403153+nana0519@users.noreply.github.com>
2022-01-30 13:13:24 +0000

    [Finish]応用課題5

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,18 @@ class User < ApplicationRecord
   def is_followed_by?(user)
     reverse_of_relationships.find_by(followed_id:user.id).present?
   end
+  
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
+  end
 end
